5e5d2f20fcb26f4a8f96e25dcd455b47f9337e70
Cody Milliron <codymilliron.dev@gmail.com>
2024-12-25 15:50:26 -0500

    Add ability to add user

---
+++ actions/auth-actions.js
@@ -0,0 +1,40 @@
+"use server";
+import { createUser } from "@/lib/user";
+import { hashUserPassword } from "@/lib/hash";
+import { redirect } from "next/navigation";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address.";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "Password must be atleast 8 characters long.";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  const hashedUserPassword = hashUserPassword(password);
+  try {
+    createUser(email, hashedUserPassword);
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email:
+            "it seems like an account for the chosen email already exists.",
+        },
+      };
+    }
+    throw error;
+  }
+
+  redirect("/training");
+}
